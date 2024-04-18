3591b310656a834ff19c1a876039c47c14d307cb
Alexey Yankov <yankovav.wm@gmail.com>
2024-04-18 18:32:04 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import "./styles/_boilerplate.scss";
 
 function App() {
   const [count, setCount] = useState(0)
